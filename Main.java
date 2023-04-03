c2d763065db6ccbf91ab9b1814f454ed9c2e4742
XiaoLilith <125671807+XiaoLilith@users.noreply.github.com>
2023-04-04 00:08:49 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,15 +1,23 @@
-class Main {
-    public static void main(String[] args) {
-        xeDap XD = new xeDap();
-        MayBay MB = new MayBay();
-        System.out.println(MB.loaiPhuongTien());
-        MB.catCanh();
-        MB.haCanh();
-        System.out.println("Van Toc:"+MB.layVanToc());
-        xeOto OT = new xeOto();
-        System.out.println(OT.loaiPhuongTien());
-        System.out.println("Van Toc:"+OT.layVanToc());
-        System.out.println(XD.loaiPhuongTien());
-        System.out.println("Van Toc:"+XD.layVanToc());
+interface shape {
+    void draw ();
+}
+class rectangle implements shape {
+    @Override
+    public void draw (){
+        System.out.println(" draw ");
+    }
+}
+class cricle implements shape {
+    @Override
+    public void draw (){
+        System.out.println(" draw ");
+    }
+}
+class bai{
+    public static void main ( String[] args) {
+        rectangle r = new rectangle ();
+        cricle c = new cricle ();
+        r.draw();
+        c.draw();
     }
 }
